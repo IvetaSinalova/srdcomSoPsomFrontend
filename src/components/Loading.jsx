5a296369faaf55e3908aad41121d1c814ecab228
Iveta Šinálová <iveta.sinalova78@gmail.com>
2024-09-26 13:48:37 +0200

    loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      <img src="logo_quido.png" alt="logo" />
-      <div class="flex items-center justify-center min-h-screen">
-        <div class="w-16 h-16 border-4 border-red border-dashed rounded-full animate-spin"></div>
-      </div>
+    <div class="flex items-center justify-center flex-col content-height">
+      {/* <img
+        className="pb-4"
+        src="logo_quido.png"
+        alt="logo"
+        style={{ width: "100px" }}
+      /> */}
+      <div class="w-16 h-16 border-t-4 border-r-4 border-red rounded-full animate-spin"></div>
     </div>
   );
 };
